24641870b4456928ca10a372cb1d7d5deb6b5200
Kushagra Singh <kushagrasinghx@gmail.com>
2025-04-08 09:27:22 +0530

    initial release

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <nav>
+        <div className="logo">cv insights</div>
+      </nav>
+      <main>
+        <div className="subtext">This application showcases the <strong>cv insights</strong> resume parser and its ability to parse information from a resume PDF.</div>
+        <div className="upload-file">
+          <div className="upload-file-subtext">
+            Browse a <strong>pdf</strong> file or drop it here
+          </div>
+          <div className="upload-file-text"><span role="img" aria-label="lock" style={{ marginRight: '6px' }}>🔒</span>File data is used locally and never leaves your browser</div>
+          <button className="browse-file-button">Browse File</button>
+        </div>
+        <div className="credits">Project by <strong>Kushagra Singh</strong> and <strong>Abhuday Sudhir</strong></div>
+      </main>
+    </>
+  )
+}
+
+export default App
